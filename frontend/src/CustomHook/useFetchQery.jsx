2acfb2a0d1f5aeae4a0a365f2b0a2bb675834cd4
vishalyadav0987 <mr.yadavvishal0987@gmail.com>
2025-01-24 15:05:56 +0530

    Add custom hook for fetching query-based questions
    - Created `useFetchQery.jsx` to implement a custom hook for fetching questions based on search queries.
    - Defined the hook to manage the state and side effects of fetching questions:
      - Used `useEffect` to trigger the fetch operation whenever `searchQuery`, `setQueryBasedQuestion`, or `page` changes.
      - Implemented `searchOnQueryHandler` as an asynchronous function to handle the API request:
        - Logged "No data found" and cleared the `queryBasedQuestion` state if the response indicates no data.
        - Updated the state with the fetched questions, total items, and total pages if the response is successful.
        - Added error handling to log any errors that occur during the API request.
    - Exported the custom hook for use in other components.
    - This custom hook provides a reusable and efficient way to manage query-based question fetching, enhancing code modularity and readability.

---
+++ frontend/src/CustomHook/useFetchQery.jsx
@@ -0,0 +1,34 @@
+import  { useEffect } from 'react'
+import { useQuestionContext } from '../QuestionContext/QuestionContext';
+import axios from 'axios';
+
+const useFetchQery = (searchQuery,page) => {
+    const { setQueryBasedQuestion,setTotalQuestion,setTotalPages
+    } = useQuestionContext();
+
+    useEffect(() => {
+        const searchOnQueryHandler = async () => {
+            if (!searchQuery) return; // Dont search if the query is empty
+            try {
+                const link = page > 0 ? `http://localhost:3000/api/v1/find?title=${searchQuery}&page=${page}`: `http://localhost:3000/api/v1/find?title=${searchQuery}`
+                const response = await axios.get(link);
+                if (!response.data.success) {
+                    console.log("No data found");
+                    setQueryBasedQuestion([]);
+                    return;
+                }
+                const data = response.data;
+                setQueryBasedQuestion(data.questions);
+                setTotalQuestion(data?.totalItems)
+                setTotalPages(data?.totalPages)
+                // console.log(data);
+            } catch (error) {
+                console.log(error);
+            }
+        };
+
+        searchOnQueryHandler();
+    }, [searchQuery, setQueryBasedQuestion,page]);
+}
+
+export default useFetchQery
