fb5531314f400c9297694933ec8ba49f877f893c
vishalyadav0987 <mr.yadavvishal0987@gmail.com>
2025-01-24 14:39:57 +0530

    Backend Complete that assign SPEAKX.

--- backend/index.js
+++ backend/index.js
@@ -3,7 +3,11 @@ const connectDB = require('./connectDB/connect');
 const app = express();
 require('dotenv').config();
 const port = 3000 || process.env.PORT;
+const cors = require('cors');
+const QuestionRoute = require('./routes/QuestionRoute')
 
+
+app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
@@ -11,6 +15,7 @@ app.use(express.urlencoded({ extended: true }));
 app.get('/test', (req, res) => {
     res.send('This is the backend server testing route');
 });
+app.use('/api/v1/questions', QuestionRoute);
 
 
 const start = async () => {
