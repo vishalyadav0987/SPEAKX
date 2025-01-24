e3a58975357ee9960df5dc3e7c17b58128f9caa4
vishalyadav0987 <mr.yadavvishal0987@gmail.com>
2025-01-24 15:02:44 +0530

    UI Changes
     - Change the color of logo using filter proprty invert the color of logo
    
     - Also change bg color of button.

--- frontend/src/components/Navbar/Navbar.jsx
+++ frontend/src/components/Navbar/Navbar.jsx
@@ -1,30 +1,31 @@
 import React from 'react'
 import Logo from '../../assets/main-logo@3x.webp'
 import './Navbar.css'
+import { Link } from 'react-router-dom'
 
 const Navbar = () => {
   return (
     <>
       <header className="header">
         <div className="header-container">
-        <a href="/" className="logo">
-                <img src={Logo} alt="logo" />
-            </a>
-       
-       
-            <nav className="nav">
-                <ul>
-                    <li><a href="/">Home</a></li>
-                    <li><a href="/about">About</a></li>
-                    <li><a href="/services">Services</a></li>
-                    <li><a href="/contact">Contact</a></li>
-                </ul>
-            </nav>
-       
-        <div className='header-button'>
+          <Link to="/" className="logo">
+            <img src={Logo} alt="logo" style={{filter:"invert(2)"}}/>
+          </Link>
+
+
+          <nav className="nav">
+            <ul>
+              <li><a href="/">Home</a></li>
+              <li><a href="/about">About</a></li>
+              <li><a href="/services">Services</a></li>
+              <li><a href="/contact">Contact</a></li>
+            </ul>
+          </nav>
+
+          <div className='header-button'>
             <button>Sign in</button>
             <button>Free Trail</button>
-        </div>
+          </div>
         </div>
       </header>
     </>
