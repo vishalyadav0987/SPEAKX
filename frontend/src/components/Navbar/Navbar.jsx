fa56f142e5ac286e758c9082286cdb3bc2843372
vishalyadav0987 <mr.yadavvishal0987@gmail.com>
2025-01-25 10:47:48 +0530

    Responsive for all devices.
    - Navbar responsiveness is complete.
    - useGsap to slider come from the right mean navbar.
    - use hamburg icon to open.
    - use cross icon to close.

--- frontend/src/components/Navbar/Navbar.jsx
+++ frontend/src/components/Navbar/Navbar.jsx
@@ -1,9 +1,30 @@
-import React from 'react'
+import React, { useEffect, useRef, useState } from 'react'
 import Logo from '../../assets/main-logo@3x.webp'
 import './Navbar.css'
 import { Link } from 'react-router-dom'
+import { GiHamburgerMenu } from "react-icons/gi";
+import { RxCross2 } from "react-icons/rx";
+import gsap from 'gsap'
 
 const Navbar = () => {
+  const [isNavOpen,setIsNavOpen] = useState(true);
+  const navBarCloseOpenRef = useRef(null)
+  const openNavBar = () => {
+    setIsNavOpen(true);
+    gsap.to(navBarCloseOpenRef.current, {
+      x: 0,
+      duration: 0.5,
+      ease: 'power3.out',
+    });
+  };
+  const closeNavBar = () => {
+    gsap.to(navBarCloseOpenRef.current, {
+      x: '100%',
+      duration: 0.5,
+      ease: 'power3.in',
+      onComplete: () => setIsNavOpen(false), // Ensure state updates after animation
+    });
+  };
   return (
     <>
       <header className="header">
@@ -26,7 +47,28 @@ const Navbar = () => {
             <button>Sign in</button>
             <button>Free Trail</button>
           </div>
+          <div className='hamburg-icon'>
+            <GiHamburgerMenu onClick={openNavBar}/>
+            </div>
         </div>
+      <div className='responsive-slider-nav'
+      ref={navBarCloseOpenRef}>
+          <div className="cross-icon" onClick={closeNavBar}>
+          <RxCross2 />
+          </div>
+          <nav className="responsive-nav">
+            <ul>
+              <li><Link to="/">Home</Link></li>
+              <li><Link to="/">About</Link></li>
+              <li><Link to="/">Services</Link></li>
+              <li><Link to="/">Contact</Link></li>
+            </ul>
+          </nav>
+          <div className='responsive-header-button'>
+            <button>Sign in</button>
+            <button>Free Trail</button>
+          </div>
+      </div>
       </header>
     </>
   )
