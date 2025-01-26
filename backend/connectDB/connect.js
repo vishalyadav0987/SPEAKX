3521689d9d04b8f6bcc09f301fb31c187eced8a6
vishalyadav0987 <mr.yadavvishal0987@gmail.com>
2025-01-26 12:01:11 +0530

    Host

--- backend/connectDB/connect.js
+++ backend/connectDB/connect.js
@@ -1,12 +1,16 @@
 const mongoose = require('mongoose');
 
 const connectDB = (URI) => {
-    return mongoose.connect(URI).then(() => {
+    return mongoose.connect(URI, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+        socketTimeoutMS: 30000, // Timeout after 30 seconds
+        connectTimeoutMS: 30000, // Connection timeout after 30 seconds
+    }).then(() => {
         console.log('Connected to the database');
     }).catch((error) => {
-        console.log('Error while connecting to the database', error.message);
-    }
-    );
+        console.error('Error while connecting to the database:', error.message);
+    });
 }
 
 module.exports = connectDB;
